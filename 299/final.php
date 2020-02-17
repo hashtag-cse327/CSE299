bd89334eba30d54b1bb2a596e6ee159d401b67c4
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2020-02-17 13:06:03 -0800

    Add files via upload

--- 299/final.php
+++ 299/final.php
@@ -2,18 +2,17 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<title>Measure distances</title>
+<title>Best Route</title>
 <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
 <script src="https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.js"></script>
 <link href="https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.css" rel="stylesheet" />
 <style>
-    body { margin: 0; padding: 0; }
-    #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+  body { margin: 0; padding: 0; }
+  #map { position: absolute; top: 0; bottom: 0; width: 100%; }
 </style>
 </head>
 <body style="text-align: center;">
-
-	<form style="margin-top: 35%;" method="POST">
+<form style="margin-top: 35%;" method="POST">
 			<input type="number" name="source" placeholder="Source"><br>
 			<input type="number" name="dest" placeholder="Destination"><br>
 			<button type="submit" name="submit">Find Best Route</button>
@@ -105,128 +104,151 @@ if(isset($_POST["submit"])){
 }
 
 ?>
-
-<div style="width: 50%;height: 70%;margin-left: 30%;" id="map"></div>
-<div id="distance" class="distance-container"></div>
-
-<script src="https://npmcdn.com/@turf/turf@5.1.6/turf.min.js"></script>
+  
+<div id="map" style="width: 50%;height: 70%;margin-left: 30%;"></div>
 <script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaWZ0aWF6YWhtZWQiLCJhIjoiY2s2YjAzOHNkMHc5djNucWpycXdiZXZpMyJ9.ngwkmBE-oRh69WSxoWBFEg';
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaWZ0aWF6YWhtZWQiLCJhIjoiY2s2YjAzOHNkMHc5djNucWpycXdiZXZpMyJ9.ngwkmBE-oRh69WSxoWBFEg';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [90.4076099395752, 23.79755276035878],
+        center: [90.40515303611754,23.79718424329992],
         zoom: 16
     });
 
-     map.on('load', function() {
-		map.addSource('pointsSource', {
-			'type': 'geojson',
-			'data': {
-			  "type": "FeatureCollection",
-			  "features": [
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "A"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40494918823242, 23.79872545056962]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "B"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40568947792053,23.798470220055645]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "C"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40640830993652,23.79812663972582]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "D"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40753483772278,23.796634508894723]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "E"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.4060971736908,23.795770635844292]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "F"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40557146072388,23.79617312286651]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "G"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40488481521606,23.796457808056232]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "H"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40515303611754,23.79718424329992]
-			      }
-			    },
-			    {
-			      "type": "Feature",
-			      "properties": {
-			      	"title": "I"
-			      },
-			      "geometry": {
-			        "type": "Point",
-			        "coordinates": [90.40444493293762,23.797429659694107]
-			      }
-			    }
-			  ]
-			}
-		});
-
-		map.addLayer({
-			'id': 'points',
-			'source': 'pointsSource',
-			'type': 'circle',
-		});
-	});
+    map.on('load', function() {
+        map.addSource('points', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        // feature for Mapbox DC
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40494918823242, 23.79872545056962]
+                        },
+                        'properties': {
+                            'title': '0',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40568947792053,23.798470220055645]
+                        },
+                        'properties': {
+                            'title': '1',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40640830993652,23.79812663972582]
+                        },
+                        'properties': {
+                            'title': '2',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40753483772278,23.796634508894723]
+                        },
+                        'properties': {
+                            'title': '3',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.4060971736908,23.795770635844292]
+                        },
+                        'properties': {
+                            'title': '4',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40557146072388,23.79617312286651]
+                        },
+                        'properties': {
+                            'title': '5',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40488481521606,23.796457808056232]
+                        },
+                        'properties': {
+                            'title': '6',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40515303611754,23.79718424329992]
+                        },
+                        'properties': {
+                            'title': '7',
+                            'icon': 'monument'
+                        }
+                    },
+                    {
+                        // feature for Mapbox SF
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [90.40444493293762,23.797429659694107]
+                        },
+                        'properties': {
+                            'title': '8',
+                            'icon': 'monument'
+                        }
+                    }
+                ]
+            }
+        });
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'points',
+            'layout': {
+                // get the icon name from the source's "icon" property
+                // concatenate the name to get an icon from the style's sprite sheet
+                'icon-image': ['concat', ['get', 'icon'], '-15'],
+                // get the title name from the source's "title" property
+                'text-field': ['get', 'title'],
+                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                'text-offset': [0, 0.6],
+                'text-anchor': 'top'
+            }
+        });
+    });
 </script>
-
 
 </body>
 </html>
